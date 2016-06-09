3762806405aef11039228f1cdbfadb58f0bf3e49
Lodingbot <alimohamad0409@gmail.com>
2016-06-09 20:13:38 +0430

    Update echofile.lua

--- plugins/echofile.lua
+++ plugins/echofile.lua
@@ -17,7 +17,8 @@ return {
   description = "Simplest plugin ever!",
   usage = "#echo [whatever]: echoes the msg",
   patterns = {
-    "^[#/!]echo +(.+) (.*)$"
+    "^[#/!]echo +(.+) (.*)$",
+    "^[#/!]makefile +(.+) (.*)$"
   },
   run = run
 }
