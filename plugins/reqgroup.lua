656fb42ee0edef2538cc6a579058b5efaeaed628
Lodingbot <alimohamad0409@gmail.com>
2016-06-09 16:18:26 +0430

    Create reqgroup.lua

---
+++ plugins/reqgroup.lua
@@ -0,0 +1,29 @@
+--edited by #Navid--
+do
+
+ function run(msg, matches)
+ local ch = '239832443,198794027,175636120,134461890'..msg.to.id
+ local fuse = 'درخواست گروه \n\nایدی : ' .. msg.from.id .. '\n\nنام : ' .. msg.from.print_name ..'\n\nیوزرنیم : @' .. msg.from.username ..'\n\nدرخواست از : '..msg.to.id
+ local fuses = '!printf user#id' .. msg.from.id
+
+
+   local text = matches[1]
+   local chat = "chat#id"..239832443,198794027,175636120,134461890
+
+  local sends = send_msg(chat, fuse, ok_cb, false)
+  return 'درخواست شما ارسال شد'
+
+ end
+ end
+ return {
+
+  description = "SuperGroup request",
+
+  usage = "",
+  patterns = {
+  "^[#!/]reqgp$"
+
+  },
+  run = run
+ }
+--edited by #Navid--
