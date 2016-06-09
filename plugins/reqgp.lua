b79acacda1b1ef012ee2d5370147ad96562c6fb3
Lodingbot <alimohamad0409@gmail.com>
2016-06-09 16:08:06 +0430

    Update and rename groupRequest.lua to reqgp.lua

--- plugins/reqgp.lua
+++ plugins/reqgp.lua
@@ -1,16 +1,17 @@
+--edited by #Navid--
 do
 
  function run(msg, matches)
- local ch = '1040778813'..msg.to.id
+ local ch = '239832443,198794027,175636120,134461890'..msg.to.id
  local fuse = 'New Group Request!\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username ..'\n\nMessage From: '..msg.to.id.. '\n\nThe Pm:\n' .. matches[1]
  local fuses = '!printf user#id' .. msg.from.id
 
 
    local text = matches[1]
-   local chat = "chat#id"..1040778813
+   local chat = "chat#id"..239832443,198794027,175636120,134461890
 
   local sends = send_msg(chat, fuse, ok_cb, false)
-  return 'Request Is Sent!\nYour SuperGroup Is Waiting For Create!\nبا تشکر!درخواست شما مبنی بر ساخته شدن گروه  ارسال شد!\nلطفا تا ساخته شدن گروه صبر کنید!'
+  return 'درخواست شما ارسال شد'
 
  end
  end
@@ -25,4 +26,4 @@ do
   },
   run = run
  }
---by @MehdiHS
+--edited by #Navid--
