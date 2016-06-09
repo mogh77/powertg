cbe328e53783aa323361a1bad0b1bc5dfbd1bbca
Lodingbot <alimohamad0409@gmail.com>
2016-06-10 00:34:48 +0430

    Update joke.lua

--- plugins/joke.lua
+++ plugins/joke.lua
@@ -1,10 +1,10 @@
+--Edited by @Navid_MrVersatile--
 local database = 'http://vip.opload.ir/vipdl/94/11/amirhmz/'
 local function run(msg)
 	local res = http.request(database.."joke.db")
 	local joke = res:split(",") 
 	return joke[math.random(#joke)]
 end
---Joke Plugin v1.0 By @mohammadarak
 return {
 	description = "500 Persian Joke",
 	usage = "!joke : send random joke",
@@ -14,3 +14,4 @@ return {
 		},
 	run = run
 }
+--Edited by @Navid_MrVersatile--
