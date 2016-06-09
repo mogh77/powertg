a835523cbec88f6e69b7b6d9b2856422212f8824
Lodingbot <alimohamad0409@gmail.com>
2016-06-10 01:14:20 +0430

    Create danestani.lua

---
+++ plugins/danestani.lua
@@ -0,0 +1,17 @@
+--Edited By @Navid_MrVersatile--
+local database = 'http://vip.opload.ir/vipdl/95/1/amirhmz/'
+local function run(msg)
+	local res = http.request(database.."danestani.db")
+	local danestani = res:split(",") 
+	return danestani[math.random(#danestani)]
+end
+return {
+	description = "500 Persian danestani",
+	usage = "!danestani : send random danestani",
+	patterns = {
+		"^[/#!]danestani$",
+		"^(danestani)$"
+		},
+	run = run
+}
+--Edited By @Navid_MrVersatile--
