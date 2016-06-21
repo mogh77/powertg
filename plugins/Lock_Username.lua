3813888860cb931d94867528576e703c30bd91bf
Lodingbot <alimohamad0409@gmail.com>
2016-06-22 01:22:06 +0430

    Update Lock_Username.lua

--- plugins/Lock_Username.lua
+++ plugins/Lock_Username.lua
@@ -12,14 +12,16 @@ local function run(msg, matches)
     end
     local chat = get_receiver(msg)
     local user = "user#id"..msg.from.id
-    if username == "yes" then
+    if username == "🔒" then
        delete_msg(msg.id, ok_cb, true)
     end
 end
  
 return {
   patterns = {
-  "@"
+  "@",
+  "@(.*)",
+  "(.*)@",
   },
   run = run
 }
