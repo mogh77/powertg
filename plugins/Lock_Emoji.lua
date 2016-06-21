1a0d1172175637714bd4f4d4e617d06b3052d525
Lodingbot <alimohamad0409@gmail.com>
2016-06-22 01:15:46 +0430

    Update Lock_Emoji.lua

--- plugins/Lock_Emoji.lua
+++ plugins/Lock_Emoji.lua
@@ -1,6 +1,6 @@
 local function run(msg)
     local data = load_data(_config.moderation.data)
-     if data[tostring(msg.to.id)]['settings']['emoji'] == 'yes' then
+     if data[tostring(msg.to.id)]['settings']['emoji'] == '🔒' then
 if not is_momod(msg) then
  delete_msg(msg.id, ok_cb, true)
 end
